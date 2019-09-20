9d872f16f4ecbbbf8d7b6a45f36268e5415d7406
Sabrina Martini <sabrina.martini@helmholtz-muenchen.de>
2019-09-20 13:29:49 +0200

    first

---
+++ sabrina_test/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import sabrina_test.config as cfg
---
+++ sabrina_test/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import sabrina_test.config as cfg
